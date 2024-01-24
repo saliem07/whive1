ee27cf9f1c1275254388fd7636b5204d99d24a7e
Sukadi99 <157588985+Sukadi99@users.noreply.github.com>
2024-01-25 07:23:43 +0800

    Add files via upload

---
+++ miner-tidecoin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-eu.rplant.xyz   /France/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Canada/
+#
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+while [ 1 ]; do
+	./cpuminer-sse2 -a yespowertide  -o stratum+tcp://stratum-asia.rplant.xyz:7059 -u TPBFH5DN83osbsNsPQZHvseZ8Hkk8ncYQ7.Tdc1 -t 8 -k --cpu-priority=5 
+	sleep 5
+done
