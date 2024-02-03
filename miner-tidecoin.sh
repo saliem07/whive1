2ff09278e8167eb6f69a18d823d84c25938731a7
saliem07 <158740548+saliem07@users.noreply.github.com>
2024-02-04 01:36:38 +0800

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -7,6 +7,6 @@
 #
 FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-	./cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-eu.rplant.xyz:7070 -u wv1qy9ll6xa68xngu7hxwugya3j0s969eq9ktml75x.Wh1 -t 4 --cpu-priority=19 
+	./cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-eu.rplant.xyz:7070 -u wv1qy9ll6xa68xngu7hxwugya3j0s969eq9ktml75x.Wh1 -t 4 --cpu-priority=5
 	sleep 5
 done
