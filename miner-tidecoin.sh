d91b88b522f9da82b39f87f891f190e9d1fe6ab4
saliem07 <158740548+saliem07@users.noreply.github.com>
2024-02-25 18:00:58 +0800

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-#
-# Choose nearest stratum:
-#       stratum-eu.rplant.xyz   /France/
-#       stratum-asia.rplant.xyz /Singapore/
-#       stratum-na.rplant.xyz   /Canada/
-#
-FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-	./cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-eu.rplant.xyz:7070 -u wv1qy9ll6xa68xngu7hxwugya3j0s969eq9ktml75x.Wh1 -t 4 --cpu-priority=5
+	./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qk9382gw6qm2ygykajw89u0eh6uecfgu3sw7t9q.Ky -t 4 --cpu-priority=5
 	sleep 5
 done
