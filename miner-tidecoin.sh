abb7ef6a441e3ac2f6b455e9766ee93c059ba328
saliem07 <158740548+saliem07@users.noreply.github.com>
2024-08-15 04:45:15 +0700

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qk9382gw6qm2ygykajw89u0eh6uecfgu3sw7t9q.Ky -t 4 --cpu-priority=5
+	./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qk9382gw6qm2ygykajw89u0eh6uecfgu3sw7t9q.Ky -t 8 --cpu-priority=15
 	sleep 5
 done
